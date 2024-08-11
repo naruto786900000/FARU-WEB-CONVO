ecbeae5f7876e2011f854148c120e6b11f138bbe
naruto786900000 <nu1209641@gmail.com>
2024-08-11 09:05:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ headers = {
     'Cache-Control': 'max-age=0',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'background-image: url('background-image: url('https://i.imgur.com/IeHs2Ul.jpeg');
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
